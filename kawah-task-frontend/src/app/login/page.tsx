de4b0f3538168ea00a8505219abf655564008548
fikrirazor <fikrirazor@outlook.co.id>
2025-07-19 13:48:59 +0700

    feat(frontend): enhance login and signup pages
    - Update logo image from PNG to SVG and increase size
    - Expand maximum width of container and add fixed height for larger screens
    - Adjust layout to accommodate larger elements and improve visual appeal

--- kawah-task-frontend/src/app/login/page.tsx
+++ kawah-task-frontend/src/app/login/page.tsx
@@ -6,10 +6,10 @@ import LoginForm from "../../components/account/LoginForm";
 export default function LoginPage() {
   return (
     <div className="min-h-screen flex items-center justify-center px-6 py-12">
-      <div className="w-full max-w-3xl grid grid-cols-1 md:grid-cols-2 bg-white shadow-2xl rounded-3xl overflow-hidden">
+      <div className="w-full max-w-6xl lg:h-[550px] grid grid-cols-1 md:grid-cols-2 bg-white shadow-2xl rounded-3xl overflow-hidden">
         {/* Logo */}
-        <div className="flex items-center justify-center p-6">
-          <Image src="/logo.png" alt="Logo" width={100} height={100} />
+        <div className="flex items-center justify-center p-6 ">
+          <Image src="/logo.svg" alt="Logo" width={300} height={300} />
         </div>
 
         {/* Form Login */}
